fb241b967ae568e8ed7e67d698ce14f75e2f9a8d
jprsurendra <jpr_surendra@yahoo.co.in>
2020-10-30 09:18:22 +0530

    Author popup

--- source_code/django_poc/web/books_app/views.py
+++ source_code/django_poc/web/books_app/views.py
@@ -1,9 +1,30 @@
+import random
+import json
+import requests
+
 from django.shortcuts import redirect
+
+from apis.authors.tasks import demo_task
 from web.common_app.custom_views import WebTemplateView
 
+BASE_URL =  'http://localhost:8000'
 
 class BooksListTemplateView(WebTemplateView):
     template_name = 'books/book_list.html'
+    last_value = 1
+
+    def new_number(self):
+        num = BooksListTemplateView.last_value
+        print("Last Number:", num)
+        res = demo_task.delay(num)
+        print(type(res))
+        # print(f"id={res.id}, state={res.state}, status={res.status} ")
+
+        # print(res.get())
+        # x = random.randint(1,9)
+        # BooksListTemplateView.last_value += x
+        # print("New Number ref: ", x)
+
 
     def dispatch(self, *args, **kwargs):
         return super(BooksListTemplateView, self).dispatch(*args, **kwargs)
@@ -14,6 +35,8 @@ class BooksListTemplateView(WebTemplateView):
         # author_name_list_api = self.fetch_api_data(api_url = '/bookapi/author-name-list/')
         # context['author_names'] = author_name_list_api #.get('data').get('results')
         context['author_names'] = []
+
+        self.new_number()
 
         return context
 
@@ -27,3 +50,27 @@ class BooksListTemplateView(WebTemplateView):
 
 class BookTemplateView(WebTemplateView):
     template_name = 'books/book_entry.html'
+
+    def call_api(self, url):
+        resp = requests.get(BASE_URL + url, cookies=self.request.COOKIES)
+        json_data = json.loads(resp.content)
+        return json_data
+
+    def dispatch(self, *args, **kwargs):
+        return super(BookTemplateView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(BookTemplateView, self).get_context_data(**kwargs)
+        try:
+            context['authors_list'] = self.call_api(url='/authorsapi/common_operations/')
+            return context
+        except Exception as e:
+            return context
+
+
+    # def post(self, request, *args, **kwargs):
+    #     user_id = request.POST.get('user_id')
+    #     return redirect('/customer-care/calendar/' + user_id)
+
+
+
